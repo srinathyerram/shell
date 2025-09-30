f99a30e99b9650dd23fd351a304e3b8d11a6447b
srinath <srinathyerram98@gamil.com>
2025-09-30 15:21:41 +0530

    sleep time-edit-v3

--- sleep-variable.sh
+++ sleep-variable.sh
@@ -4,4 +4,4 @@ START_TIME=$(date +%s)
 sleep 10 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIMETI-$START_TIME))
-echo "Script executed in:"$TOTAL_TIME seconds"
+echo "Script executed in: $TOTAL_TIME seconds"
