ed7b2c7de8bdcd6bf3ff7b49ffcbe745852c71c9
srinath <srinathyerram98@gamil.com>
2025-10-21 17:44:48 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+
+if [ $NUMBER -lt 100]; then
+echo "Given number $NUMBER is less than 100"
+else
+echo "Given number $NUMBER is greater than or equal to 100"
+fi 
+
+
+# -gt
+# eq
+# -ne
