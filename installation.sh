15f3ef129ddcb9d2bd3400f065f46dda2f553be3
srinath <srinathyerram98@gamil.com>
2025-10-23 11:53:02 +0530

    installation.sh

--- installation.sh
+++ installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
 echo "Please run this script as root or using sudo."
+    exit 1
 fi 
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
   echo "MySQL installation failed. Please check your network connection and package manager settings."
+  exit 1
   else 
     echo "MySQL installed successfully."
     fi 
